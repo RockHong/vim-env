a7af75a92eafc346fe1e197bba949edaa4de4721
Zhihua Hong <zhihua.hong@sap.com>
2014-04-16 01:30:27 -0400

    refine install.sh

--- install.sh
+++ install.sh
@@ -1,50 +1,39 @@
-#todo: add the content of example readme from the post in vimcasts.org
-#
-#—
-#not use the submodule strategy..
-#don't run git clone inside bunlde, or you will add a submodule
-#
-#
-#ln -s vimrc ~/.vimrc    # right?
-#ln -s ctags ...      !!! use full path when create slink
-#
-#
-#ctags configuration file doesn't support comment...
-#one option a line in ctags configuration file
-#
-#
-# 安装exuberant ctags 
-# 因为zypper搜不到；所以下个源代码包； ./configure   make    make install
+#!/bin/bash
 
+# check we are in right directory when running this script
+# if the parent directory is ~/.vim, then we are pretty fine.
+# if not,
+#     if ~/.vim exists, mv it as ~/.vim.old
+#     if ~/.vimrc and ~/.gvimrc exist, rename them into ~/.vimrc.old and ~/.gvimrc.old
+#     copy the parent directory to ~, make its name .vim
+SCRIPT_PATH=`readlink -f $0`
+CURRENT_DIR=`dirname $SCRIPT_PATH`
+HOME_DIR=`readlink -f ~`
+if [ $CURRENT_DIR != $HOME_DIR/.vim ];then
+  if [ -e ~/.vim ]; then
+    mv ~/.vim ~/.vim.old
+  fi
+  if [ -e ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.old
+  fi
+  if [ -e ~/.gvimrc ]; then
+    mv ~/.gvimrc ~/.gvimrc.old
+  fi
 
-Installation:
+  cp -r $CURRENT_DIR ~/.vim
+fi
 
-    git clone git://github.com/nelstrom/dotvim.git ~/.vim
+# check if system has already has the RIGHT version of ctags
+# can we find it in zyyper, if so use zypper
+# if not, then download source, ./configure, make, make install
+ctags --version | grep -i Exuberant > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Exuberant ctags doesn't exist. please install using zypper or from source"
+fi
 
-Create symlinks:
-
-    ln -s ~/.vim/vimrc ~/.vimrc
-    ln -s ~/.vim/gvimrc ~/.gvimrc
-
-Switch to the `~/.vim` directory, and fetch submodules:
-
-    cd ~/.vim
-    git submodule init
-    git submodule update
-
-
-
-#check we are in right directory
-
-#check/install ctags
-
-#if ~/.vim exists, rename it
-
-#if ~/.vimrc and ~/.gvimrc exist, rename them
-
-#cp the repository as ~/.vim
-
-#create symbol links for .vimrc, .gvimrc, and .ctags
-
+# create symbol links for .vimrc, .gvimrc, and .ctags
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ~/.vim/ctags ~/.ctags
 
 
