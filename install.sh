23ec679452d5516f33e6c3575dad4058ee839e0a
Hong <hongzhihua@gmail.com>
2014-04-20 16:38:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-# check we are in right directory when running this script
-# if the parent directory is ~/.vim, then we are pretty fine.
-# if not,
-#     if ~/.vim exists, mv it as ~/.vim.old
-#     if ~/.vimrc and ~/.gvimrc exist, rename them into ~/.vimrc.old and ~/.gvimrc.old
-#     copy the parent directory to ~, make its name .vim
-SCRIPT_PATH=`readlink -f $0`
+#SCRIPT_PATH=`readlink -f $0`   # readlink -f doesn't work on mac
+#HOME_DIR=`readlink -f ~`
+SCRIPT_PATH=$(x=`dirname $0`; cd $x; y=`pwd`; z=`basename $0`; echo $y/$z)
+HOME_DIR=$(x=`dirname ~`; cd $x; y=`pwd`; z=`basename ~`; echo $y/$z)
 CURRENT_DIR=`dirname $SCRIPT_PATH`
-HOME_DIR=`readlink -f ~`
+
+
+if [ -e ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc.old
+fi
+if [ -e ~/.gvimrc ]; then
+  mv ~/.gvimrc ~/.gvimrc.old
+fi
+if [ -e ~/.ctags ]; then
+  mv ~/.ctags ~/.ctags.old
+fi
+
+
+# create symbol links for .vimrc, .gvimrc, and .ctags
+ln -s $CURRENT_DIR/vimrc ~/.vimrc
+ln -s $CURRENT_DIR/gvimrc ~/.gvimrc
+ln -s $CURRENT_DIR/ctags ~/.ctags
+
+# create symbol link for .vim if needed
 if [ $CURRENT_DIR != $HOME_DIR/.vim ];then
-  if [ -e ~/.vim ]; then
+  if [ -h ~/.vim ]; then
+      rm -rf ~/.vim
+  elif [ -e ~/.vim ]; then
     mv ~/.vim ~/.vim.old
   fi
-  if [ -e ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.old
-  fi
-  if [ -e ~/.gvimrc ]; then
-    mv ~/.gvimrc ~/.gvimrc.old
-  fi
-
-  cp -r $CURRENT_DIR ~/.vim
+  ln -s $CURRENT_DIR ~/.vim
 fi
 
 # check if system has already has the RIGHT version of ctags
@@ -42,13 +52,8 @@ if [ $? -ne 0 ]; then
 #  echo "ruby doesn't exist. please install it, or command-t won't work."
 else # assume that if vim with "+ruby" then ruby is installed
   ( cd ~/.vim/bundle/command-t/ruby/command-t ; ruby extconf.rb ; make || 
-    echo "command-t is fully installed. please fix it manually" ; make clean )
+    echo "command-t is not fully installed. please read the online manual and fix it" ; make clean )
 fi
-
-
-# create symbol links for .vimrc, .gvimrc, and .ctags
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-ln -s ~/.vim/ctags ~/.ctags
+# on mac the above compilation command may not work, due to ruby version and clang
 
 
